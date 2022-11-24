80a49d538d13f68e1869985206cfc6afca612849
Aminu Rabiu <arabiu033@gmail.com>
2022-11-24 14:48:20 +0100

    Add a spinner to display when fetching data

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Dna } from 'react-loader-spinner';
+
+const Spinner = () => (
+  <div className="spinner">
+    <Dna />
+  </div>
+);
+
+export default Spinner;
