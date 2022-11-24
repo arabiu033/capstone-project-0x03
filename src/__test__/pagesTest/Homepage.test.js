3ea016635d5871b3f5939cf8ba04671c829ff8e2
Aminu Rabiu <arabiu033@gmail.com>
2022-11-24 19:09:02 +0100

    Add tests for pages created

---
+++ src/__test__/pagesTest/Homepage.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import store from '../../redux/configureStore';
+import Homepage from '../../pages/Hompage';
+
+test('renders correctly Header component', () => {
+  const home = renderer.create(
+    <BrowserRouter>
+      <Provider store={store}>
+        <Homepage setTitle={() => 'func'}/>
+      </Provider>
+    </BrowserRouter>
+  ).toJSON();
+  expect(home).toMatchSnapshot();
+});
